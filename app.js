51ddeff47017119c74510504d22952b4811c93fd
Edouard FERRARI <epi.doud@gmail.com>
2016-10-22 07:44:03 +0000

    feat(App): crappy app.js ... but usable

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-
-var express = () => {
-  
-};
-var routes = require('./routes/index');
-var add = require('./routes/add');
+var async = require('async');
 
 var app = express();
 
@@ -19,67 +13,85 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // getting-started.js
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-  
-  db.schemas = {};
-  db.schemas.card = mongoose.Schema({
-  username: String
-  , password: String
-  });
-  
-});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(function (req, res, next) {
-  req.db = { mongoose: mongoose };
-  next();
-});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/add', add);
+var routes = (callback) => {
+  var routes = require('./routes/index');
+  var add = require('./routes/add');
+  
+  app.use('/', routes);
+  app.use('/add', add);
+};
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+var mongodb = (callback) => {
+  var mongoose = require('mongoose');
+  mongoose.connect('mongodb://localhost/test');
+  
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    // we're connected!
+    
+    db.schemas = {};
+    db.schemas.card = mongoose.Schema({
+    username: String
+    , password: String
+    });
+    
+    app.use(function (req, res, next) {
+      req.db = { mongoose: mongoose };
+      next();
+    });
+    
+    return callback();
+  });
+};
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+var handleAppError = (callback) => {
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+  
+  // error handlers
+  
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+  
+  // production error handler
+  // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: {}
     });
   });
-}
+  
+  return callback();
+};
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+var jobs = [routes, mongodb, handleAppError];
+async.series(jobs, () => console.log("App loaded"));
 
 
 module.exports = app;
